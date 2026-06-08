88de7fe87ee6495d94300f75bb0fa7803754c39d
Herdif <herdisat29@gmail.com>
2026-06-08 16:08:06 +0700

    fix: remove unused ts-expect-error directive

--- components/wallet/WagmiProviderWrapper.tsx
+++ components/wallet/WagmiProviderWrapper.tsx
@@ -2,7 +2,6 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider, createConfig, http } from 'wagmi'
-// @ts-expect-error - TS sometimes fails to resolve viem's ESM chains bundle in next15
 import { mainnet, base, arbitrum, optimism } from 'viem/chains'
 
 // Create a client
