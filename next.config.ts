371d440b17c3cf628cd4d60932ad8473ab7fc029
Herdif <herdisat29@gmail.com>
2026-06-10 22:09:29 +0700

    Fix proxy export for Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        "pino-pretty": false,
-        "@react-native-async-storage/async-storage": false,
-      };
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
